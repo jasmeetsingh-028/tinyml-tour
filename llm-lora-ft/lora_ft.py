d8463890eccfed04d0463432eca2286b10d117f3
jasmeetsingh-028 <sjasmeet135@gmail.com>
2026-06-29 20:06:29 +0530

    feat: dataset loading and trainer configs

--- llm-lora-ft/lora_ft.py
+++ llm-lora-ft/lora_ft.py
@@ -1,4 +1,5 @@
 import torch
+import yaml
 from datasets import Dataset
 
 
@@ -16,7 +17,23 @@ tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 
 # get dataset
 
-train_ds, eval_ds = get_medical_resoning_sft(DATASET_NAME, tokenizer)
+#train_ds, eval_ds = get_medical_resoning_sft(DATASET_NAME, tokenizer)
 
-print(train_ds[0]["text"])
-print(f"Train size: {len(train_ds)}, Eval size: {len(eval_ds)}")
+# print(train_ds[0]["text"])
+# print(f"Train size: {len(train_ds)}, Eval size: {len(eval_ds)}")
+
+#load lora config
+
+with open("configs/config.yaml") as f:
+    configs = yaml.safe_load(f)
+
+# ----- LoRA config -----
+lora_config = LoraConfig(**configs["lora"])
+
+
+# ----- SFT config -----
+sft_config =  SFTConfig(
+    **configs["sft"],
+    bf16=torch.cuda.is_available() 
+)
+
